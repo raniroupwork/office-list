631d3f34188673511d3dd2bf246d60e740f43430
errec <errecme@gmail.com>
2021-02-13 11:38:35 -0300

    Add theme base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#989EA7',
+        'primary-light': '#E8EDF3',
+        'primary-dark': '#313E4F',
+        'accent-blue': '#33A6BA',
+        'accent-red': '#FF7B92',
+        'background-color': '#FFFFFF',
+      },
+    },
   },
   variants: {
     extend: {},
