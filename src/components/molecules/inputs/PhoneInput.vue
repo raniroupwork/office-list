0fb1c39a1afba967488bb14ad20249160527b7f3
errec <errecme@gmail.com>
2021-02-19 22:06:47 -0300

    Update input layout

---
+++ src/components/molecules/inputs/PhoneInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <label class="block">
+    <span class="text-gray-700">Input</span>
+    <input class="form-input mt-1 block w-full" :type="type" :placeholder="placeholder">
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'TextInput',
+  props: {
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+  },
+};
+
+</script>
--- src/stories/molecules/inputs/GenericInput.stories.jsx
+++ src/stories/molecules/inputs/GenericInput.stories.jsx
@@ -8,9 +8,30 @@ export default {
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { GenericInput },
-  template: '<GenericInput></GenericInput>',
+  template: '<GenericInput :placeholder="placeholder" :required="required" :type="type" :label="label" :error="error"></GenericInput>',
 });
 
 export const Name = Template.bind({});
 Name.args = {
+  placeholder: '',
+  required: true,
+  type: 'text',
+  label: 'Full name',
+};
+
+export const Email = Template.bind({});
+Email.args = {
+  placeholder: 'name@example.com',
+  required: true,
+  type: 'email',
+  label: 'E-mail',
+};
+
+export const TitleError = Template.bind({});
+TitleError.args = {
+  placeholder: '',
+  required: true,
+  type: 'text',
+  label: 'Title',
+  error: true,
 };
